ccec01e3757c84b3e69e9cd87a2aba64efb488df
Victor Lannes <victor.ferreiralannes@hotmail.com>
2023-03-14 21:30:24 -0300

    Add project files.

---
+++ ProjetoMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Victor Lannes";
+            home.Email = "victor.ferreiralannes@hotmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
